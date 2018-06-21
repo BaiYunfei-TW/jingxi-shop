a4c7badd69fb2e0819319bf19f254c6d6185b1d0
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-06-21 12:41:32 +0800

    add swagger configuration

--- src/main/java/cn/cooode/jingxishop/controller/ProductController.java
+++ src/main/java/cn/cooode/jingxishop/controller/ProductController.java
@@ -4,6 +4,8 @@ import cn.cooode.jingxishop.entity.Inventory;
 import cn.cooode.jingxishop.entity.Product;
 import cn.cooode.jingxishop.repository.InventoryRepository;
 import cn.cooode.jingxishop.repository.ProductRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.net.URI;
 import java.util.Date;
 import java.util.List;
 
+@Api(value = "onlinestore", description = "Operations pertaining to products in Jingxi Shop")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -43,12 +46,14 @@ public class ProductController {
         return ResponseEntity.status(204).build();
     }
 
+    @ApiOperation(value = "View a product with the given id", response = Product.class)
     @GetMapping("/{id}")
     public ResponseEntity get(@PathVariable Long id) {
         Product product = productRepository.getById(id);
         return ResponseEntity.ok(product);
     }
 
+    @ApiOperation(value = "View a list of products", response = Iterable.class)
     @GetMapping("")
     public ResponseEntity getAll(@RequestParam(defaultValue = "") String name, @RequestParam(defaultValue = "") String description) {
         List<Product> products = productRepository.findAllByNameContainingAndDescriptionContaining(name, description);
